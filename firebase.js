c2efbad734df3b59789c70a1ac2acb82dee5f0cc
Jonathan Ng <jonng1234@gmail.com>
2021-07-29 20:51:06 -0700

    Fixed bugs and added firebase

--- firebase.js
+++ firebase.js
@@ -6,6 +6,12 @@ import "@firebase/storage";
 // Your web app's Firebase configuration, which you copy-pasted from Step 6
 const firebaseConfig = {
   // CONFIG INFO GOES HERE
+  apiKey: "AIzaSyB7RrROnr1nm_hJCCfwtJKw4RSWkCVjBKY",
+  authDomain: "chapsnat-jon.firebaseapp.com",
+  projectId: "chapsnat-jon",
+  storageBucket: "chapsnat-jon.appspot.com",
+  messagingSenderId: "229140744092",
+  appId: "1:229140744092:web:7aa2717d3141a800efaa59"
 };
 // Initialize Firebase
 
