17414a8d9bbe2ee5c3bf2eabb7076aec8dd9b825
Jonathan Ng <jonng1234@gmail.com>
2021-08-12 16:59:12 -0700

    Deleted More Unused Code

--- screens/map_screen/MapScreen.js
+++ screens/map_screen/MapScreen.js
@@ -42,7 +42,6 @@ export default function MapScreen() {
 	const [currLocation, setCurrLocation] = useState(null);
 	const mapView = useRef(null);
 	const [bitmojiFrame, setBitmojiFrame] = useState(0);
-	const [isJustDismissed, setIsJustDismissed] = useState(false);
 
 	useEffect(() => {
 		(async () => {
